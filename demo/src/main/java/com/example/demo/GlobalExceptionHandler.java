adc9055b133b5992882c1c1e99b859ebd8f6fb5c
FatFace <shurko-2003@yandex.ru>
2024-05-09 18:03:10 +0300

    added exception handler

---
+++ demo/src/main/java/com/example/demo/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationException(MethodArgumentNotValidException ex) {
+        return new ResponseEntity<>(ex.getBindingResult().getFieldError().getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
